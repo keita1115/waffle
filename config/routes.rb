2016fba363d88340393997cc5ba2e8591215b8b7
keita1115 <keita.hashimoto1115@gmail.com>
2022-11-14 11:44:09 +0000

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ devise_for :customers,skip: [:passwords], controllers: {
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root :to =>"homes#top"
+
 end
