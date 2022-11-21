d1656616a421d5bda6d9589ea4eda741490fcda3
keita1115 <keita.hashimoto1115@gmail.com>
2022-11-21 10:16:02 +0000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,32 @@ Rails.application.routes.draw do
       sessions: "admin/sessions"
   }
 
+namespace :admin do
+    root to: 'homes#top'
+    resources :items
+    resources :genres
+    resources :customers
+    resources :orders
+    resources :order_details
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => "homes#about"
+    resources :addresses
+    resources :orders do
+      post 'order/confirm' => 'orders#confirm'
+      get 'orders/complete' => 'orders#complete'
+    end
+    resources :cart_items, only: [:index, :create, :update, :destroy] do
+      delete 'cart_items/all_destroy' => 'cart_items#all_destroy'
+    end
+    resources :items
+    get 'customers/mypage' => 'customers#show'
+    get 'customers/mypage/edit' => 'customers#edit'
+    patch 'customers/mypage' =>  'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+    patch 'customers/withdrawal' => 'customers#withdrawal', as: 'withdrawal'
+  end
+  
 end
