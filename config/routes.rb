1a1dcfb8f3a33cd0d9775c7a1950abbd8f7bf16d
keita1115 <keita.hashimoto1115@gmail.com>
2022-11-21 10:17:39 +0000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-   # 顧客用
+  # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
       registrations: "public/registrations",
       sessions: 'public/sessions'
   }
+  
   # 管理者用
   # URL /admin/sign_in ...
     devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
