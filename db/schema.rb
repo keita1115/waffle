388557c339cd38f286bb70ba38bc1bd1b2a3688c
keita1115 <keita.hashimoto1115@gmail.com>
2022-11-23 08:55:28 +0000

    addresses migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_063649) do
+ActiveRecord::Schema.define(version: 2022_11_23_085330) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "post_number"
+    t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
