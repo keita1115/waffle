b3036540ce154fe16dfb7398ff5df5e70e743249
keita1115 <keita.hashimoto1115@gmail.com>
2022-12-05 05:57:09 +0000

    orders/model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  has_many :order_details
+  has_many :customer_orders, dependent: :destroy
+  belongs_to :customer
+
+  enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
+  enum order_status: { "wait_payment": 0, "confirm_payment": 1, "making": 2, "preparing_ship": 3, "finish_prepare": 4}
+
+  def with_tax_price
+      (price * 1.1).floor
+  end
+
 end
