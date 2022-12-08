cdc827747e60fce204463ceb2b7a593805e61619
keita1115 <keita.hashimoto1115@gmail.com>
2022-12-08 10:13:03 +0000

    items/

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,25 @@
 class Public::ItemsController < ApplicationController
+  
+  def index
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.page(params[:page]).per(6)
+    else
+      @items = Item.all.page(params[:page]).per(6)
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+    @genres = Genre.all
+  end
+
+
+  private
+    def item_params
+      params.require(:item).permit(:genre_id, :name, :item_image, :introduction, :price, :is_active)
+    end
+    
 end
