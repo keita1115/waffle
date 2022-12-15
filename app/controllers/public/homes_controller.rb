9b67745881e2136c041c2c7106269202e95cd6be
keita1115 <keita.hashimoto1115@gmail.com>
2022-12-15 10:24:12 +0000

    homes/top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,12 @@
 class Public::HomesController < ApplicationController
+  def top
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.page(params[:page]).per(3)
+    else
+      @items = Item.all.page(params[:page]).per(3)
+    end
+    @genres = Genre.all
+  end
+  
 end
