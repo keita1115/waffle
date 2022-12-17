40cdb63fca88268ece457e8b014a4190fd4879e9
keita1115 <keita.hashimoto1115@gmail.com>
2022-12-17 07:20:55 +0000

    orders

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,26 @@
 class Admin::OrderDetailsController < ApplicationController
+  
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_details_params)
+    @order = @order_detail.order
+
+    if @order_detail.making_status == "making"
+      @order.update(order_status: 2)
+      redirect_to admin_order_path(@order.id)
+    elsif @order.order_details.count == @order.order_details.where(making_status: "complete").count
+
+      @order.update(order_status: 3)
+      redirect_to admin_order_path(@order.id)
+    else
+      redirect_to admin_order_path(@order.id)
+    end
+  end
+
+
+  private
+    def order_details_params
+      params.require(:order_detail).permit(:order_id, :item_id, :price, :quantity, :making_status)
+    end
+
 end
