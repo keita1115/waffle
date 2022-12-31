ffaa4cb7e6ac26844006b0aeaba303fe661f8555
keita1115 <keita.hashimoto1115@gmail.com>
2022-12-31 05:00:35 +0000

    order_detail/model

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,12 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order, dependent: :destroy
+  belongs_to :item
+
+  #製作ステータスのenum
+  enum making_status: { impossible_making: 0, waiting_for_making: 1, making: 2, complete: 3 }
+
+  def with_tax_price
+      (price * 1.1).floor
+  end
+
 end
